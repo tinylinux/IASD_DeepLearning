091d6f87b2af8d402c3f3401054b50eb6c3dd64e
Rida <rlali@crans.org>
2022-11-30 19:06:26 +0100

    MobileNet

--- golois.py
+++ golois.py
@@ -13,8 +13,8 @@ N = 10000
 epochs = 350
 batch = 128
 # filters = 32
-filters = 40
-trunk = 40
+filters = 32
+trunk = 32
 
 input_data = np.random.randint(2, size=(N, 19, 19, planes))
 input_data = input_data.astype ('float32')
@@ -40,7 +40,7 @@ input = keras.Input(shape=(19, 19, planes), name='board')
 x = layers.Conv2D(trunk, 1, padding='same', kernel_regularizer=regularizers.l2(0.0001))(input)
 x = layers.BatchNormalization()(x)
 x = layers.ReLU()(x)
-for i in range (0):
+for i in range (50):
     # Mobile Net Way
     m = layers.Conv2D(filters, (1,1), kernel_regularizer=regularizers.l2(1e-4), use_bias=False)(x)
     m = layers.BatchNormalization()(m)
@@ -51,7 +51,7 @@ for i in range (0):
     x = layers.Conv2D(trunk, (1,1), kernel_regularizer=regularizers.l2(1e-4), use_bias=False)(x)
     x = layers.BatchNormalization()(x)
     x = layers.Add()([m,x])
-for i in range(100):
+for i in range(0):
     # Residual Way
     x1 = layers.Conv2D(filters, 5, padding='same')(x)
     x1 = layers.BatchNormalization()(x1)
